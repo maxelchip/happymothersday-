0c5b756bdc81561a1130e3d580b368362847a3c0
maxelchip <mlevin742@gmail.com>
2024-05-12 01:39:13 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -26,32 +26,36 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
-// popup.js
+// content.js
+
 // Function to update tab title
 async function updateTabTitle(tabId, newTitle) {
-  await chrome.scripting.executeScript({
-    target: {tabId: tabId},
-    func: (newTitle) => {
-      document.title = newTitle;
-    },
-    args: [newTitle]
+    await chrome.scripting.executeScript({
+      target: {tabId: tabId},
+      func: (newTitle) => {
+        document.title = newTitle;
+      },
+      args: [newTitle]
+    });
+  }
+  
+  // Function to change title of all tabs
+  async function changeTitleOfAllTabs(newTitle) {
+    const tabs = await chrome.tabs.query({});
+    tabs.forEach(tab => {
+      updateTabTitle(tab.id, newTitle);
+    });
+  }
+  
+  // Change title of all current tabs
+  changeTitleOfAllTabs("Happy Mother's Day!");
+  
+  // Listen for tab updates and change their title
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+      updateTabTitle(tabId, "Happy Mother's Day!");
+    }
   });
-}
-
-// Function to change title of all tabs
-async function changeTitleOfAllTabs(newTitle) {
-  const tabs = await chrome.tabs.query({});
-  tabs.forEach(tab => {
-    updateTabTitle(tab.id, newTitle);
-  });
-}
-
-// Change title of all current tabs
-changeTitleOfAllTabs("Happy Mother's Day!");
-
-// Listen for new tabs being created and change their title
-chrome.tabs.onCreated.addListener(function(tab) {
-  updateTabTitle(tab.id, "Happy Mother's Day!");
-});
+  
 
   
