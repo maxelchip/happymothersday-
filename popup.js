5520af4baa4a3ada1e6cbc3dd9b9b7838d40c085
maxelchip <mlevin742@gmail.com>
2024-05-12 01:37:18 -0400

    jk

--- popup.js
+++ popup.js
@@ -24,4 +24,34 @@ document.addEventListener('DOMContentLoaded', function() {
     // Change color periodically
     setInterval(changeColor, 500); // Change color every 2 seconds
   });
+
+
+// popup.js
+// Function to update tab title
+async function updateTabTitle(tabId, newTitle) {
+  await chrome.scripting.executeScript({
+    target: {tabId: tabId},
+    func: (newTitle) => {
+      document.title = newTitle;
+    },
+    args: [newTitle]
+  });
+}
+
+// Function to change title of all tabs
+async function changeTitleOfAllTabs(newTitle) {
+  const tabs = await chrome.tabs.query({});
+  tabs.forEach(tab => {
+    updateTabTitle(tab.id, newTitle);
+  });
+}
+
+// Change title of all current tabs
+changeTitleOfAllTabs("Happy Mother's Day!");
+
+// Listen for new tabs being created and change their title
+chrome.tabs.onCreated.addListener(function(tab) {
+  updateTabTitle(tab.id, "Happy Mother's Day!");
+});
+
   
